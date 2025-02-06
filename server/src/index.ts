eb58bb41ccc1fd02cd5a3ec954f8c9ac8ad6897d
Jose Tellez <jdrgv999@gmail.com>
2025-02-05 19:01:45 -0500

    Refactor: removing some logs

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,9 +12,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(typeof process.env.DB_PASSWORD)
-console.log(process.env.DB_PASSWORD)
-
 // PostgreSQL connection
 const pool = new Pool({
     user: process.env.DB_USER,
