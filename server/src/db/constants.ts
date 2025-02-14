31bb9a6b388faf8d0e298230e7993b05f2d06087
Jose Tellez <jdrgv999@gmail.com>
2025-02-14 16:21:44 -0500

    Feat: Adding the definitions for the pages table

--- server/src/db/constants.ts
+++ server/src/db/constants.ts
@@ -13,3 +13,17 @@ export const USER_COLUMNS = {
     CREATED_AT: 'created_at',
     UPDATED_AT: 'updated_at'
 };
+
+export const PAGES_COLUMNS = {
+    ID: 'id',
+    NAME: 'name',
+    CREATION_AT: 'creation_at',
+    LAST_MODIFIED: 'last_modified',
+    ICON: 'icon',
+    COVER_IMAGE: 'cover_image',
+    USER_ID: 'user_id',
+    PARENT_PAGE_ID: 'parent_page_id',
+    IS_FAVORITE: 'is_favorite',
+    DESCRIPTION: 'description',
+    STATUS: 'status'
+};
