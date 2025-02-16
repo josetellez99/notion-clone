5a9a5a46562ae25d92ef90345f77d61f4e3fec4d
Jose Tellez <jdrgv999@gmail.com>
2025-02-16 06:41:13 -0500

    Feat: Adding the pages router to the index server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import 'module-alias/register';
 import express from 'express';
 import cors from 'cors';
 import router from "@/routes/auth.routes";
+import {router as pagesRouter} from '@/routes/pages';
 import { pool } from "@/db/pool"
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/auth", router);
+app.use("/pages", pagesRouter)
 
 // Test database connection
 pool.connect()
